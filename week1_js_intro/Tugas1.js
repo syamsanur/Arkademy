68145a1cd633c85d064304dcdd229f9e558a849f
syamsa nur <syamsanur0@gmail.com>
2020-08-07 11:29:53 +0700

    tugas1

--- week1_js_intro/Tugas1.js
+++ week1_js_intro/Tugas1.js
@@ -1,1 +1,44 @@
-console.log("test");
+const biodata = {
+    name: "Raden Syamsa Nur Ashari Mahmud",
+    age : 24,
+    hobbies : [
+        "Travelling",
+        "Gaming",
+    ],
+    isMarried:false,
+    schoolList :[
+        {
+            name: "SMA Laboratorium UPI",
+            yearIn: 2010,
+            yearOut: 2013,
+            major: "IPS"
+        },
+        {
+            name: "UNJANI",
+            yearIn: 2013,
+            yearOut: 2019,
+            major: "Informatika"
+        }
+    ],
+    skills: [
+        {
+            skillName: "Php",
+            level : "beginner"
+        },
+        {
+            skillName: "CodeIgniter",
+            level : "beginner"
+        },
+        {
+            skillName: "Laravel",
+            level : "beginner"
+        },
+        {
+            skillName: "Bootstrap",
+            level : "beginner"
+        }
+    ],
+    interestInCoding : true
+}
+
+console.log(biodata)
