22f7453469169cf19c790f3e4488330272a9533d
syamsa nur <syamsanur0@gmail.com>
2020-08-08 19:20:45 +0700

    2

--- week1_js_intro/Tugas2.js
+++ week1_js_intro/Tugas2.js
@@ -1,4 +1,35 @@
-let bahasaIndonesia = 80;
-let bahasaInggris = 75;
-let matematika = 67;
-let ipa = 87;
+let bIndo =56
+let bIng =45
+let mtk =56
+let ipa =78
+
+let total = null
+let nilai = null
+let grade
+
+//console.log(typeof (bIng || bIndo || mtk || ipa) )
+console.log(typeof (bIng && bIndo && mtk && ipa) )
+
+
+if(typeof (bIng && bIndo && mtk && ipa) === 'number' ) {
+
+    total = bIng+bIndo+mtk+ipa
+    nilai = total/4
+
+    if(nilai <=100 && nilai >=90){
+        grade= "A"
+    }else if( nilai <=89 && nilai >=80){
+        grade ="B"
+    }else if( nilai <=79 && nilai >=70){
+        grade="C"
+    }else if( nilai <=69 && nilai >=60){
+        grade="D"
+    }else{
+        grade="E"
+    }
+
+    console.log(`nilai ${nilai} dan grade ${grade}`)
+
+}else{
+    console.log("data tidak boleh kosong dan harus angka")
+}
