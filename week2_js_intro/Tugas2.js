cc8be31d7d48439546b771c780c713079724abba
syamsa nur <syamsanur0@gmail.com>
2020-08-11 16:47:43 +0700

    23

--- week2_js_intro/Tugas2.js
+++ week2_js_intro/Tugas2.js
@@ -7,74 +7,24 @@ const name = [
 ]
 
 getName = (x,y,callback) =>{
-    let nameLow = name.map(v => v.toLowerCase()); //kecilkan
-    let filter =nameLow.filter(e => e.includes(x)) //buang element yang tidak mengandung kata d cari
-    if( y <= filter.length){ // apa kepanjangan?
-        let result = filter.slice(0,y) //potong array dari index[0] sampe index[y]
-        let result2 =result.map(i => i.charAt(0).toUpperCase()+ i.slice(1)) //besarkan huruf awal 
-        callback(result2)//kirim k callback
+    let result 
+     result = name.map(v => v.toLowerCase());
+     result =result.filter(e => e.includes(x))
+    if(result.length == 0){
+        console.log("nama tidak d temukan")
     }else{
-        console.log("ooray panjang teuing atau nama tidak ditemukan")
+        if(y<result.length){
+             result = result.slice(0,y) 
+             result =result.map(i => i.charAt(0).toUpperCase()+ i.slice(1)) //besarkan huruf awal 
+             callback(result)
+        }else{
+            console.log("ooray panjang teuing")
+        }
     }
-    
 }
 
 printName = (result)=>{
     console.log(result)//tampikan hasil
 }
 
-getName("an",0,printName)
-
-
-
-//console.log(string.includes(substring));
-
-/*
-getName = (x) => {
-    nameLow = name.map(v => v.toLowerCase());
-    let hasil= []    
-    for (let i = 0; i < nameLow.length; i++){
-        if( nameLow[i].includes(x)){
-            hasil.push(nameLow[i])
-            
-        }
-    }
-    console.log(hasil)
-}
-
-getName("an")
-
-
-for ( i=0; i<3; i++){ //y
-    console.log(nameLow[i])
-}
-
-
-let i
-let j
-
-let hasil =[]
-nameLow = name.map(v => v.toLowerCase());
-
-getName = (x,y) =>{
-    let filter =nameLow.filter(e => e.includes(x))
-    console.log(filter)
-
-    for (i=0; i<y; i++){
-        hasil.push(filter[i])
-    }
-    
-    console.log(hasil)
-}
-
-getName("an",6)
-
-
-
-
-*/
-
-//x kata d cari
-//y batas
-
-
+getName("an",3,printName)
--- week2_js_intro/Tugas3.js
+++ week2_js_intro/Tugas3.js
@@ -1,15 +1,22 @@
-
-
-seleksiNilai =(nilaiAwal, nilaiAkhir, dataArray) => {
-    if((nilaiAwal < nilaiAkhir) && (dataArray.length >=5)){
-        hasil= dataArray.filter(i=> i> nilaiAwal && i<nilaiAkhir)
-        hasil.sort()
-        console.log(hasil)
-    
-
+seleksiNumber = (nilaiAwal,nilaiAkhir,dataArray) =>{
+    let hasil
+    if(nilaiAwal>nilaiAkhir){
+        hasil = "Nilai awal tidak boleh lebih kecil dari nilai akhir"
     }else{
-        console.log(`Nilai Akhir harus lebih besar dari nilai awal`)
+        if(dataArray.length < 5){
+            hasil="data array kurang panjang"
+        }else{
+            let filter = dataArray.filter( i=>{
+                return i>nilaiAwal && i<nilaiAkhir
+            })
+            if(filter.length ==0){
+                hasil="tidak ada yang sesuai"
+            }else{
+                hasil= filter.sort()
+            }
+        }
     }
+     console.log(hasil)
 }
 
-seleksiNilai(1,5,[8,2,9,5,1,4,3])
+seleksiNumber(1,5,[2,3,6,1,5,8,9,1,2,5,3,4])
