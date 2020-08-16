53db66a29b87334a20e472e258268bcded5f4af7
syamsa nur <syamsanur0@gmail.com>
2020-08-16 20:44:10 +0700

    final

--- week2_js_intro_2/Tugas4.js
+++ week2_js_intro_2/Tugas4.js
@@ -1,33 +1,33 @@
 const fetch = require("node-fetch");
 const url = "https://jsonplaceholder.typic.com/users";
 
-// fetch(url)
-//   .then((res) => res.json())
-//   .then((res) => res.map((user) => user.name))
+fetch(url)
+  .then((res) => res.json())
+  .then((res) => res.map((user) => user.name))
   
-//   .then((user) => console.log(user))
+  .then((user) => console.log(user))
   
-//   .catch(err => console.log("error"))
+  .catch(err => console.log("error"))
 
 
 
 
-fetchData = async (url) =>{
-    const res = await fetch(url) //var res tunggu proses fetch
-    return res.json() //return res dalam bentuk json
-}
+// fetchData = async (url) =>{
+//     const res = await fetch(url) //var res tunggu proses fetch
+//     return res.json() //return res dalam bentuk json
+// }
 
-printData = async () =>{
-    try{
-        const hasil = await fetchData(url) //hasil tunggu proses fetch
-        name = hasil.map(i=> i.name)
-        console.log(name)
-    }catch(err){
-        console.log("geh")
-    }
-}
+// printData = async () =>{
+//     try{
+//         const hasil = await fetchData(url) //hasil tunggu proses fetch
+//         name = hasil.map(i=> i.name)
+//         console.log(name)
+//     }catch(err){
+//         console.log("wat?")
+//     }
+// }
 
-printData()
+// printData()
 
   
   
